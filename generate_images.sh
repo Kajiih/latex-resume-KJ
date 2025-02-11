1dc01fd03a0b182315ef36a6d89e737f4a9c2cb3
Julian Paquerot <111422357+Kajiih@users.noreply.github.com>
2025-02-11 15:56:21 +0100

    🔨 Separate mains for both languages

--- generate_images.sh
+++ generate_images.sh
@@ -2,8 +2,8 @@
 
 # Define paths and filenames
 IMAGES_PATH="images"
-FINAL_NAME="resume_latex_kj"
-PDF_FILE="main.pdf"
+FINAL_NAME_FRENCH="resume-fr"
+FINAL_NAME_ENGLISH="resume-en"
 
 # Create images directory if it doesn't exist
 mkdir -p "$IMAGES_PATH"
@@ -11,19 +11,35 @@ mkdir -p "$IMAGES_PATH"
 # Clear out old images
 rm -rf "$IMAGES_PATH"/*
 
-# Convert PDF to PNG images with ImageMagick
-if magick -density 300 "$PDF_FILE" -background white -alpha remove -quality 90 "$IMAGES_PATH/$FINAL_NAME.png"; then
-    echo "PDF successfully converted to PNG images."
+# Convert French PDF to PNG images with ImageMagick
+if magick -density 300 "$FINAL_NAME_FRENCH.pdf" -background white -alpha remove -quality 90 "$IMAGES_PATH/$FINAL_NAME_FRENCH.png"; then
+    echo "French PDF successfully converted to PNG images."
 else
-    echo "Failed to convert PDF to PNG." >&2
+    echo "Failed to convert French PDF to PNG." >&2
     exit 1
 fi
 
-# Copy the PDF to the images folder
-if cp main.pdf "$IMAGES_PATH/$FINAL_NAME.pdf"; then
-    echo "PDF copied successfully."
+# Convert English PDF to PNG images with ImageMagick
+if magick -density 300 "$FINAL_NAME_ENGLISH.pdf" -background white -alpha remove -quality 90 "$IMAGES_PATH/$FINAL_NAME_ENGLISH.png"; then
+    echo "English PDF successfully converted to PNG images."
 else
-    echo "Failed to copy PDF." >&2
+    echo "Failed to convert English PDF to PNG." >&2
+    exit 1
+fi
+
+# Copy the French PDF to the images folder
+if cp "$FINAL_NAME_FRENCH.pdf" "$IMAGES_PATH/$FINAL_NAME_FRENCH.pdf"; then
+    echo "French PDF copied successfully."
+else
+    echo "Failed to copy French PDF." >&2
+    exit 1
+fi
+
+# Copy the English PDF to the images folder
+if cp "$FINAL_NAME_ENGLISH.pdf" "$IMAGES_PATH/$FINAL_NAME_ENGLISH.pdf"; then
+    echo "English PDF copied successfully."
+else
+    echo "Failed to copy English PDF." >&2
     exit 1
 fi
 
